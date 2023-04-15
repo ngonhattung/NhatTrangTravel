ceb49804bd440d9a59b6c85d87c2674d20e45ee6
ngonhattung <ngonhattung08062003@gmail.com>
2023-04-15 17:33:31 +0700

    dieu chinh pg3

---
+++ js/pg3.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.img-slider-pg3').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
+  });
+      
