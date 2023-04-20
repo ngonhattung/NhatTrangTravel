7d3472bd1669e7c327c5ee34e5ce0143e63e2ca1
ngonhattung <ngonhattung08062003@gmail.com>
2023-04-20 21:30:59 +0700

    add pg1 diemden

---
+++ js/slider-bg-diemden.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.backgroup-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:2000,
+        dots:true,
+    });
+  });
+      
