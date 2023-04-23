be2d539ccc47896fed25998d0864f4cc5a0355eb
ngonhattung <ngonhattung08062003@gmail.com>
2023-04-23 21:55:58 +0700

    dieu chinh pg diemden

--- js/slider-bg-diemden.js
+++ js/slider-bg-diemden.js
@@ -9,4 +9,15 @@ $(document).ready(function(){
         dots:true,
     });
   });
+
+  $(document).ready(function(){
+    $('.slider-pg4-diemden').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
+  });
+      
       
